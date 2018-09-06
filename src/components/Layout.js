a8a2da4a76481d197578d8059a5fd71f2d9a3492
Riki Sholiardi <sholiardy@gmail.com>
2018-09-06 16:22:29 +0700

    io-connection client

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,6 +1,29 @@
 import React, { Component } from 'react';
+import io from 'socket.io-client';
+
+const socketUrl = 'http://192.168.56.1:3231';
 
 class Layout extends Component {
+  
+  constructor(props) {
+    super(props);
+    this.state = {
+      socket: null
+    };
+  }
+  
+  componentWillMount() {
+    this.initSocket()
+  }
+
+  initSocket = () => {
+    const socket = io(socketUrl)
+    socket.on('connect', () => {
+      console.log("Connected")
+    })
+    this.setState({socket})
+  }
+
   render() {
 
     const { title } = this.props
